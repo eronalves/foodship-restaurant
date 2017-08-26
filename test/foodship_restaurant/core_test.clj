c3f8e98a98b84130969d9017a9c6e48119daa686
Eron <eronra@gmail.com>
2017-08-26 20:31:57 -0300

    Deleting silly test

--- test/foodship_restaurant/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns foodship-restaurant.core-test
-  (:require [clojure.test :refer :all]
-            [foodship-restaurant.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
