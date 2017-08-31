683f80e9f7d3adb3045c117b017e7e0a0d3eca9a
Eron <eronra@gmail.com>
2017-08-31 16:03:45 -0300

    Adding domain controller test

--- test/foodship_restaurant/domain/controller_test.clj
+++ test/foodship_restaurant/domain/controller_test.clj
@@ -1,12 +1,50 @@
 (ns foodship-restaurant.domain.controller-test
   (:require 
     [clojure.test :refer :all]
+    [bond.james :as bond :refer [with-stub!]]
     [foodship-restaurant.component-fixture :refer [setup-custom-with-lifecycle component]]
+    [foodship-restaurant.adapters.db-adapter :as db-adapter]
+    [foodship-restaurant.domain.domain-functions.filter-restaurants :as filter-restaurants]
     [foodship-restaurant.domain.controller :as controller]))
 
 (use-fixtures :once (setup-custom-with-lifecycle {:db {}}))
 
-; (deftest get-restaurant 
-;   (testing "must be return the info of restaurant"
-;     (is (= {:name "America"} (controller/get-restaurant component 1)))))
+(def restaurant
+  {:restaurant/name "Mexicaníssimo" :restaurant/tags ["mexican"]})
+
+(def restaurants
+  [{:restaurant/name "Mexicaníssimo" :restaurant/tags ["mexican"]}
+   {:restaurant/name "Aoyama" :restaurant/tags ["japanese"]}
+   {:restaurant/name "Sancho Pancho" :restaurant/tags ["spanish"]}
+   {:restaurant/name "Aokitama" :restaurant/tags ["japanese"]}])
+
+(def filtered-restaurants
+  [{:restaurant/name "Mexicaníssimo" :restaurant/tags ["mexican"]}])
+
+(deftest get-restaurant
+  (testing "must be return the info of restaurant"
+    (with-stub! [[db-adapter/retrieve (fn [component id] restaurant)]]
+      (let [resultset (controller/get-restaurant component 1)]
+        (is (= 1 (-> db-adapter/retrieve bond/calls count)))
+        (is (= restaurant resultset)))))
+
+  (testing "must be return nil when not found restaurant"
+    (with-stub! [[db-adapter/retrieve (fn [component id] nil)]]
+      (let [resultset (controller/get-restaurant component 2)]
+        (is (= 1 (-> db-adapter/retrieve bond/calls count)))
+        (is (= nil resultset))))))
+
+(deftest restaurants
+  (testing "must be return filtered list of restaurants"
+    (with-stub! [[db-adapter/all (fn [component] restaurants)]
+                 [filter-restaurants/by-name-and-tags (fn [restaurants name tags] filtered-restaurants)]]
+      (let [resultset (controller/restaurants component "mex" ["mexican"])
+            calls-filter-restaurants (bond/calls filter-restaurants/by-name-and-tags)
+            args-filter-restaurants (:args (get calls-filter-restaurants 0))]
+        (is (= 1 (-> db-adapter/all bond/calls count)))
+        (is (= 1 (count calls-filter-restaurants)))
+        (is (= restaurants (nth args-filter-restaurants 0)))
+        (is (= "mex" (nth args-filter-restaurants 1)))
+        (is (= ["mexican"] (nth args-filter-restaurants 2)))
+        (is (= filtered-restaurants resultset))))))
 
