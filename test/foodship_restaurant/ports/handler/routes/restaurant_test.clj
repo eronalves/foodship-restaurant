a3fb1846d5bef456c3354ba3420264e180ef34e2
Eron <eronra@gmail.com>
2017-09-05 15:09:01 -0300

    Adding restaurant routes tests

--- test/foodship_restaurant/ports/handler/routes/restaurant_test.clj
+++ test/foodship_restaurant/ports/handler/routes/restaurant_test.clj
@@ -15,6 +15,12 @@
    {:name "Sancho Pancho" :tags ["spanish"]}
    {:name "Aokitama" :tags ["japanese"]}])
 
+(def filtered-restaurants-coll
+  [{:name "Mexicaníssimo" :tags ["mexican"]}
+   {:name "Aokitama" :tags ["japanese"]}])
+
+(def retrieved-restaurant {:name "Mexicaníssimo" :tags ["mexican"]})
+
 (deftest restaurants
   (testing "GET '/restaurants' endpoint without params must return a list of restaurants"
     (with-stub! [[controller/restaurants (fn [component name tags] restaurants-coll)]]
@@ -27,4 +33,30 @@
         (is (nil? (nth args-restaurants 1)))
         (is (nil? (nth args-restaurants 2)))
         (is (= 200 (:status response)))
-        (is (= restaurants-coll body))))))
+        (is (= restaurants-coll body)))))
+
+  (testing "GET '/restaurants' endpoint with params must return a filtered list of restaurants"
+    (with-stub! [[controller/restaurants (fn [component name tags] filtered-restaurants-coll)]]
+      (let [response (util/mock-get app (str restaurant-route "?name=i&tags=mexican&tags=japanese"))
+            body     (util/parse-body (:body response))
+            calls-restaurants (bond/calls controller/restaurants)
+            args-restaurants (:args (get calls-restaurants 0))]
+        (is (= 1 (count calls-restaurants)))
+        (is (= {} (nth args-restaurants 0)))
+        (is (= "i" (nth args-restaurants 1)))
+        (is (= ["mexican", "japanese"] (nth args-restaurants 2)))
+        (is (= 200 (:status response)))
+        (is (= filtered-restaurants-coll body))))))
+
+(deftest restaurant
+  (testing "GET '/restaurant/:id endpoint with id"
+    (with-stub! [[controller/get-restaurant (fn [component id] retrieved-restaurant)]]
+      (let [response (util/mock-get app (str restaurant-route "/12"))
+            body (util/parse-body (:body response))
+            calls-restaurant (bond/calls controller/get-restaurant)
+            args-restaurant (:args (get calls-restaurant 0))]
+        (is (= 1 (count calls-restaurant)))
+        (is (= {} (nth args-restaurant 0)))
+        (is (= 12 (nth args-restaurant 1)))
+        (is (= 200 (:status response)))
+        (is (= retrieved-restaurant body))))))
