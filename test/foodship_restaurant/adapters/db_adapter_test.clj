ee7279d3e3aa902923b59108d1c7eec80f32ea46
Eron <eronra@gmail.com>
2017-09-11 11:20:23 -0300

    Adding db adapter create and update tests

--- test/foodship_restaurant/adapters/db_adapter_test.clj
+++ test/foodship_restaurant/adapters/db_adapter_test.clj
@@ -3,18 +3,15 @@
     [clojure.test :refer :all]
     [bond.james :as bond :refer [with-stub!]]
     [foodship-restaurant.component-fixture :refer [setup-custom-with-lifecycle component]]
-    [foodship-restaurant.ports.db.memory_db :as mapgraph]
+    [foodship-restaurant.ports.db.memory_db :as memory-db]
     [foodship-restaurant.adapters.db-adapter :as db-adapter]))
 
 (use-fixtures :once (setup-custom-with-lifecycle {:memory-db {}}))
 
-(def restaurant-mapgraph
-  {:restaurant/name "Mexicaníssimo" :restaurant/tags ["mexican"]})
-
 (def restaurant
   {:name "Mexicaníssimo" :tags ["mexican"]})
 
-(def restaurants-mapgraph
+(def restaurants-memory-db
   [{:restaurant/name "Mexicaníssimo" :restaurant/tags ["mexican"]}
    {:restaurant/name "Aoyama" :restaurant/tags ["japanese"]}
    {:restaurant/name "Sancho Pancho" :restaurant/tags ["spanish"]}
@@ -28,16 +25,37 @@
 
 (deftest retrieve
   (testing "must return a restaurant with the id parameter"
-    (with-stub! [[mapgraph/get-restaurant (fn [component id] restaurant)]]
+    (with-stub! [[memory-db/get-restaurant (fn [component id] restaurant)]]
       (let [resultset (db-adapter/retrieve-restaurant component 1)]
-        (is (= 1 (-> mapgraph/get-restaurant bond/calls count)))
+        (is (= 1 (-> memory-db/get-restaurant bond/calls count)))
         (is (= restaurant resultset))))))
 
 (deftest all
   (testing "must return a list of all restaurants"
-    (with-stub! [[mapgraph/all (fn [component] restaurants-mapgraph)]]
+    (with-stub! [[memory-db/all (fn [component] restaurants-memory-db)]]
       (let [resultset (db-adapter/all-restaurants component)]
-        (is (= 1 (-> mapgraph/all bond/calls count)))
+        (is (= 1 (-> memory-db/all bond/calls count)))
         (is (= restaurants resultset))))))
+
+(deftest create-restaurant!
+  (testing "must create a restaurant calling memory db"
+    (with-stub! [[memory-db/insert-restaurant! (fn [component restaurant])]]
+      (let [resultset (db-adapter/create-restaurant! component restaurant)
+            calls-insert-restaurant (bond/calls memory-db/insert-restaurant!)
+            args-insert-restaurant (:args (get calls-insert-restaurant 0))]
+        (is (= 1 (count calls-insert-restaurant)))
+        (is (= {} (nth args-insert-restaurant 0)))
+        (is (= restaurant (nth args-insert-restaurant 1)))))))
+
+(deftest create-restaurant!
+  (testing "must create a restaurant calling memory db"
+    (with-stub! [[memory-db/insert-restaurant! (fn [component restaurant])]]
+      (let [resultset (db-adapter/update-restaurant! component restaurant)
+            calls-insert-restaurant (bond/calls memory-db/insert-restaurant!)
+            args-insert-restaurant (:args (get calls-insert-restaurant 0))]
+        (is (= 1 (count calls-insert-restaurant)))
+        (is (= {} (nth args-insert-restaurant 0)))
+        (is (= restaurant (nth args-insert-restaurant 1)))))))
+
 
 
