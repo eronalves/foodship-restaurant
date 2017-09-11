665e6d1db61450861a331fa42041741a5b72bbe0
Eron <eronra@gmail.com>
2017-09-11 14:03:51 -0300

    Refactoring create and update route schema and adding route tests

--- src/foodship_restaurant/ports/handler/routes/route_schemas.clj
+++ src/foodship_restaurant/ports/handler/routes/route_schemas.clj
@@ -6,26 +6,26 @@
    (s/optional-key :tags) [s/Str]})
 
 (s/defschema CreateAlterRestaurant
-  {:restaurant/id s/Int
-   :restaurant/name s/Str
-   :restaurant/tags [s/Keyword]
-   :restaurant/preparation-time {:preparation-time/min s/Int
-                                 :preparation-time/max s/Int}
-   :restaurant/business-hours {:business-hours/day s/Str
-                               :business-hours/opening s/Str
-                               :business-hours/closing s/Str}
-   (s/optional-key :restaurant/location) {:location/addres s/Str
-                                          :location/number s/Int
-                                          :location/neighborhood s/Str
-                                          :location/city s/Int
-                                          :location/state s/Int
-                                          :location/country s/Int
-                                          :location/zipcode s/Str}
-   (s/optional-key :restaurant/menu) [{:menu/title s/Str
-                                       :menu/category s/Str
-                                       :menu/ingredients [s/Str]
-                                       :menu/price s/Num
-                                       :menu/serves s/Int}]
-   (s/optional-key :restaurant/banking) {:banking/agency s/Int
-                                         :banking/account s/Str
-                                         :banking/digit s/Int}})
+  {(s/optional-key :id) s/Int
+   :name s/Str
+   :tags [s/Str]
+   :preparation-time {:min s/Int
+                                 :max s/Int}
+   :business-hours [{:day s/Keyword
+                     :opening s/Str
+                     :closing s/Str}]
+   (s/optional-key :location) {:addres s/Str
+                               :number s/Int
+                               :neighborhood s/Str
+                               :city s/Int
+                               :state s/Int
+                               :country s/Int
+                               :zipcode s/Str}
+   (s/optional-key :menu) [{:title s/Str
+                            :category s/Str
+                            :ingredients [s/Str]
+                            :price s/Num
+                            :serves s/Int}]
+   (s/optional-key :banking) {:agency s/Int
+                              :account s/Str
+                              :digit s/Int}})
--- test/foodship_restaurant/ports/handler/routes/restaurant_test.clj
+++ test/foodship_restaurant/ports/handler/routes/restaurant_test.clj
@@ -21,7 +21,7 @@
 
 (def retrieved-restaurant {:name "Mexicaníssimo" :tags ["mexican"]})
 
-(deftest restaurants
+(deftest list-restaurants
   (testing "GET '/restaurants' endpoint without params must return a list of restaurants"
     (with-stub! [[controller/restaurants (fn [component name tags] restaurants-coll)]]
       (let [response (util/mock-get app restaurant-route)
@@ -48,7 +48,7 @@
         (is (= 200 (:status response)))
         (is (= filtered-restaurants-coll body))))))
 
-(deftest restaurant
+(deftest get-restaurant
   (testing "GET '/restaurant/:id endpoint with id"
     (with-stub! [[controller/get-restaurant (fn [component id] retrieved-restaurant)]]
       (let [response (util/mock-get app (str restaurant-route "/12"))
@@ -60,3 +60,43 @@
         (is (= 12 (nth args-restaurant 1)))
         (is (= 200 (:status response)))
         (is (= retrieved-restaurant body))))))
+
+(def restaurant-to-create
+  {:name "Mexicaníssimo"
+   :tags ["mexican"]
+   :preparation-time {:min 45 :max 60}
+   :business-hours [{:day :monday :opening "09:00" :closing "22:00"}]})
+
+(deftest create-restaurant
+  (testing "POST '/restaurants' must create a restaurant"
+    (with-stub! [[controller/create-restaurant! (fn [component restaurant] retrieved-restaurant)]]
+      (let [response (util/mock-post app restaurant-route restaurant-to-create)
+            body (util/parse-body (:body response))
+            calls-create-restaurant (bond/calls controller/create-restaurant!)
+            args-create-restaurant (:args (get calls-create-restaurant 0))]
+        (is (= 1 (count calls-create-restaurant)))
+        (is (= {} (nth args-create-restaurant 0)))
+        (is (= restaurant-to-create (nth args-create-restaurant 1)))
+        (is (= 200 (:status response)))
+        (is (= retrieved-restaurant body))))))
+
+(def restaurant-to-update
+  {:id 1
+   :name "Mexicaníssimo"
+   :tags ["mexican"]
+   :preparation-time {:min 45 :max 60}
+   :business-hours [{:day :monday :opening "09:00" :closing "22:00"}]})
+
+(deftest update-restaurant
+  (testing "POST '/restaurants/:id' endpoint must update a restaurant"
+    (with-stub! [[controller/update-restaurant! (fn [component id restaurant] retrieved-restaurant)]]
+                (let [response (util/mock-post app (str restaurant-route "/1") restaurant-to-update)
+                      body (util/parse-body (:body response))
+                      calls-update-restaurant (bond/calls controller/update-restaurant!)
+                      args-update-restaurant (:args (get calls-update-restaurant 0))]
+                  (is (= 1 (count calls-update-restaurant)))
+                  (is (= {} (nth args-update-restaurant 0)))
+                  (is (= 1 (nth args-update-restaurant 1)))
+                  (is (= restaurant-to-update (nth args-update-restaurant 2)))
+                  (is (= 200 (:status response)))
+                  (is (= retrieved-restaurant body))))))
